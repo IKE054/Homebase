f98857fed568d99fa81b3f38a8cfb2b12bbb44b3
Mikoshi54 <Mikoshi54@Wacka54noMacBook-puro.local>
2019-02-16 16:50:32 +0900

    fix feather devise

--- db/migrate/20190216053612_devise_create_users.rb
+++ db/migrate/20190216053612_devise_create_users.rb
@@ -32,6 +32,8 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
       # t.string   :unlock_token # Only if unlock strategy is :email or :both
       # t.datetime :locked_at
 
+      # add custom
+      t.string :name,              null: false, default: ""
 
       t.timestamps null: false
     end
