04a3dae22e11c2d0898c507c01a157aba617d84f
IKE054 <upperlake.k@gmail.com>
2019-02-18 12:19:28 +0000

    fix devise

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -18,6 +18,9 @@ class Users::SessionsController < Devise::SessionsController
   #   super
   # end
 
+  def after_sign_in_path_for(resource)
+    home_index_path
+  end
   # protected
 
   # If you have extra params to permit, append them to the sanitizer.
