714ec82ba5c8e32ad9dd12a9ce264c1724db9b02
IKE054 <upperlake.k@gmail.com>
2019-02-18 05:48:44 +0000

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,39 @@
 Rails.application.routes.draw do
+  devise_for :users, :controllers => {
+    :registrations => 'users/registrations',
+    :sessions => 'users/sessions'
+  }
+
+  devise_scope :user do
+    get "sign_in", :to => "users/sessions#new"
+    get "sign_out", :to => "users/sessions#destroy"
+  end
+
+  # get 'rooms/item_create'
+  # get 'rooms/item_destroy'
+  # get 'rooms/show'
+  # get 'rooms/new'
+  # get 'rooms/create'
+  # get 'home/top'
+  get 'home/index'
+  # get 'users/edit'
+  # get 'users/show'
+  # get 'users/update'
+  # get 'items/edit'
+  # get 'items/show'
+  # get 'items/update'
+  # get 'items/create'
+  # get 'items/new'
   devise_for :users
-  # devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # home
+  root to: 'home#top'
+
+  # users
+  resources :users, only: [:show, :edit, :update]
+  # items
+  resources :items, only: [:new, :create, :show, :edit, :update]
+
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,56 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_16_053612) do
+ActiveRecord::Schema.define(version: 2019_02_17_084313) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "follows", force: :cascade do |t|
+    t.bigint "item_id"
+    t.string "tag_title", null: false
+    t.string "type_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["item_id"], name: "index_follows_on_item_id"
+  end
+
+  create_table "items", force: :cascade do |t|
+    t.bigint "user_id"
+    t.string "title", null: false
+    t.string "caption", null: false
+    t.string "category", null: false
+    t.string "url", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_items_on_user_id"
+  end
+
+  create_table "rooms", force: :cascade do |t|
+    t.bigint "user_id"
+    t.string "room_name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_rooms_on_user_id"
+  end
+
+  create_table "rooms_items", force: :cascade do |t|
+    t.bigint "room_id"
+    t.bigint "item_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["item_id"], name: "index_rooms_items_on_item_id"
+    t.index ["room_id"], name: "index_rooms_items_on_room_id"
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.bigint "item_id"
+    t.string "tag_title", null: false
+    t.string "type_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["item_id"], name: "index_tags_on_item_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -28,8 +74,18 @@ ActiveRecord::Schema.define(version: 2019_02_16_053612) do
     t.inet "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name", null: false
+    t.string "introduction"
+    t.text "profile_image"
+    t.string "header_image", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "follows", "items"
+  add_foreign_key "items", "users"
+  add_foreign_key "rooms", "users"
+  add_foreign_key "rooms_items", "items"
+  add_foreign_key "rooms_items", "rooms"
+  add_foreign_key "tags", "items"
 end
