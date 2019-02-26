254352476ed77e25ae061a3bad3a57b7393447f4
IKE054 <upperlake.k@gmail.com>
2019-02-26 03:29:21 +0000

    [create]ユーザー編集・詳細機能

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,25 @@
 class UsersController < ApplicationController
   def edit
+  	@user = User.find(params[:id])
   end
 
   def show
+    @items = Item.where(user_id: current_user.id)
   end
 
   def update
+  	@user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to user_path(current_user)
+    else
+      render :edit
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(
+    	:name, :introduction, :email, :profile_image, :header_image)
+
   end
 end
--- db/migrate/20190217084313_add_column_to_users.rb
+++ db/migrate/20190217084313_add_column_to_users.rb
@@ -3,7 +3,7 @@ class AddColumnToUsers < ActiveRecord::Migration[5.2]
 
       add_column :users, :name, :string, null: false
   	  add_column :users, :introduction, :string
-  	  add_column :users, :profile_image, :text
-  	  add_column :users, :header_image, :string
+  	  add_column :users, :profile_image_id, :string
+  	  add_column :users, :header_image_id, :string
   end
 end
