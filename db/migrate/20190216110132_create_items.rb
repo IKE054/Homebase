b150c385927508f7a0e36ffffc02c99dcbff5b8e
IKE054 <upperlake.k@gmail.com>
2019-02-26 04:24:52 +0000

    [fixed]items table

--- db/migrate/20190216110132_create_items.rb
+++ db/migrate/20190216110132_create_items.rb
@@ -2,12 +2,16 @@ class CreateItems < ActiveRecord::Migration[5.2]
   def change
     create_table :items do |t|
 
-      t.references :user, foreign_key: true
-      t.string :title, null: false
-      t.string :caption, null: false
+      # t.references :user, foreign_key: true
+       t.integer :user_id
+      t.string :title
+      t.string :caption
       t.string :category, null: false
-      t.string :url, null: false
+      t.string :item_image_id
+      t.string :url
       t.timestamps
     end
   end
 end
+
+
