3813d5802f6384d9f96ad5d3391ebae7a7b4e2b8
IKE054 <upperlake.k@gmail.com>
2019-02-26 04:23:22 +0000

    [fixed]アイテム投稿機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -9,8 +9,27 @@ class ItemsController < ApplicationController
   end
 
   def create
+    @item = Item.new(item_params)
+    @item.user_id = current_user.id
+    if @item.category.nil?
+      @item.category = '1'
+    end
+    if @item.save
+      render json: {message: 'success', itemId: @item.id}, status: 200
+    else
+      # render json: { error: @item.errors.full_messages.join(", ")}, status: 400
+      render :new
+    end
   end
 
   def new
+    @item = Item.new
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(
+      :user_id, :title, :caption, :category, :item_image, :url,
+      tags_attributes: [:id, :item_id, :tag_title, :tag_type_id, :_destroy])
   end
 end
