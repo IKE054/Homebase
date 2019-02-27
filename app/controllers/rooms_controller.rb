3a865b16a6fb5697f1d36ba577dd2eed242e20e4
IKE054 <upperlake.k@gmail.com>
2019-02-27 14:56:32 +0000

    [create remove]ルーム作成機能・不要ビュー削除

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -9,8 +9,20 @@ class RoomsController < ApplicationController
   end
 
   def new
+    @room = Room.new
   end
 
   def create
+    @room = Room.new(room_params)
+    @room.user_id = current_user.id
+    @room.save
+    redirect_to user_path(current_user.id)
+  end
+
+  private
+  def room_params
+    params.require(:room).permit(
+     :room_name, :room_image)
   end
 end
+
